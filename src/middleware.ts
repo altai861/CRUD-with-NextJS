9b3ffc4e7165099367b68362eaa6fa447f91064e
altai861 <altaizandanbileg@gmail.com>
2023-08-22 16:00:05 +0800

    Initial Commit

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+
+const allowedOrigins = process.env.NODE_ENV === 'production'
+    ? ['https://www.yoursite.com', 'https:/yoursite.com']
+    : ['http://localhost:3000']
+
+export function middleware(request: Request) {
+    
+    const origin = request.headers.get('origin')
+    console.log(origin)
+
+    if (origin && !allowedOrigins.includes(origin)) {
+        return new NextResponse(null, {
+            status: 400,
+            statusText: "Bad request",
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        })
+    }
+
+    const regex = new RegExp('/api/*')
+
+    if (regex.test(request.url)) {
+
+    }
+
+    console.log('Middleware!')
+    console.log(request.method)
+    console.log(request.url)
+
+    
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: '/api/:path*',
+}
